75065b20df219e17275189adf428fa62822cf0fb
rootofpower <kurilyakoleksii12@gmail.com>
2025-11-22 01:02:09 +0100

    implemented controlled recording

--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,12 @@
 from speech_to_text import SpeechToText
 from pynput.keyboard import Key, Listener
-
+import threading
+import time
+from datetime import datetime
+from speech_to_text.controlled_recorder import ControlledRecorder
 stt = SpeechToText(model_size="small")
-# result = stt.transcribe_file("../audio/test_audio.ogg")
-
-# print(f"Transcribed Text: {result['text']}")
+result = stt.transcribe_file("/home/rootofpower/personal/pygovno/projects/speech_to_text/audio/test.ogg")
+print(f"Transcribed Text: {result['text']}")
 
 # result_mic = stt.transcribe_from_microphone()
 
@@ -15,4 +17,30 @@ stt = SpeechToText(model_size="small")
 # print(f"Transcribed Text from Microphone until Silence: {result_mic['text']}")
 
 
+# i need a two threads one for listening for key presses and one for transcribing,
+# if i press 's' it starts transcribing from microphone until i press 'q' to stop it
 
+# timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+# recording_filename = f"recording_{timestamp}.wav"
+
+# recorder = ControlledRecorder(filename=recording_filename)
+
+# try: 
+#     while True:
+#         cmd = input("Press 's' to start recording, 'q' to stop recording and transcribe: ").strip().lower()
+#         if cmd == 's':
+#             recorder.start_recording()
+#         elif cmd == 'q':
+#             if recorder.recordingState:
+#                 recorder.stop_recording()
+#                 print("Transcribing the recorded audio...")
+#                 result = stt.transcribe_file(recording_filename)
+#                 print(f"Transcribed Text: {result['text']}")
+#             print("ended transcription.")
+#             break
+#         else:
+#             print("not exisiting command.")
+# except KeyboardInterrupt:
+#     print("Exiting the program.")
+#     if recorder.recordingState:
+#         recorder.stop_recording()        
